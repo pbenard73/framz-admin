d155c33ba1c08f8275916bfb8a9925db2919f8d4
BENARD Patrick <pbenard73@gmail.com>
2020-10-13 22:54:24 +0200

    first commit

---
+++ middlewares/adminAcl.js
@@ -0,0 +1,17 @@
+import { database, aclManager, createError } from "framz"
+
+export default type => (req, res, next) => {
+    const modelName = req.params.modelName
+
+    if (database.hasUrlModel(modelName) === false) {
+        return next()
+    }
+
+    const role = `ROLE_${modelName.toUpperCase()}_${type.toUpperCase()}`
+
+    if (aclManager.isGranted(req, role) === false) {
+        return next(createError(403))
+    }
+
+    return next()
+}
