f3f233751e251b48b75f68e3c977bd618b3c0413
BENARD Patrick <pbenard73@gmail.com>
2020-10-19 11:11:57 +0200

    test_arch

--- src/front/Admin.js
+++ src/front/Admin.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react"
 
-import Login from './Login'
+import Login from "./Login"
 
 class Admin extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log('CSTR', props)
+    }
     render() {
         return (
             <div>
                 <h1>Admin Area</h1>
                 <Login />
 
-		<ul>
-		{this.props.models.map(model => (
-			<li key={model}>{model}</li>
-		))}
-		</ul>
+                <ul>
+                    {this.props.models.map(model => (
+                        <li key={model}>{model}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
