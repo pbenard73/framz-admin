a66832798873230bd65bd2484c35246d12b1af1f
BENARD Patrick <pbenard73@gmail.com>
2020-10-19 10:50:42 +0200

    test_front_arch

--- src/front/Login.js
+++ src/front/Login.js
@@ -3,6 +3,10 @@ import React from "react"
 import framz from "./hocs/framz"
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
     loginBindThis() {
         this.props.api.call("login")
     }
